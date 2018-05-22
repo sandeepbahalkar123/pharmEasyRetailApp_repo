9dba7b0fd6721bf46a2b8ffcd9c43298403c286e
Ganesh Shirole <ganesh9405@gmail.com>
2018-05-22 18:43:46 +0530

    small change

--- app/src/main/java/com/scorg/farmaeasy/ui/fragments/ShortBookFragment.java
+++ app/src/main/java/com/scorg/farmaeasy/ui/fragments/ShortBookFragment.java
@@ -28,7 +28,7 @@ public class ShortBookFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_day_book, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_short_book, container, false);
 
         return rootView;
     }
