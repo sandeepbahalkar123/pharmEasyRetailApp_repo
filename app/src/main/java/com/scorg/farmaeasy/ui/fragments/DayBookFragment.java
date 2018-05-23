1631d01a79a2d6fa20ab0ae4409c9e5b540c7785
Ganesh Shirole <ganesh9405@gmail.com>
2018-05-23 12:38:16 +0530

    added some icons

--- app/src/main/java/com/scorg/farmaeasy/ui/fragments/DayBookFragment.java
+++ app/src/main/java/com/scorg/farmaeasy/ui/fragments/DayBookFragment.java
@@ -5,13 +5,75 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.scorg.farmaeasy.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DayBookFragment extends Fragment {
+
+    @BindView(R.id.fromDateValue)
+    TextView fromDateValue;
+    @BindView(R.id.fromDateMainLayout)
+    LinearLayout fromDateMainLayout;
+    @BindView(R.id.toDateValue)
+    TextView toDateValue;
+    @BindView(R.id.toDateMainLayout)
+    LinearLayout toDateMainLayout;
+    @BindView(R.id.selectDatelayout)
+    LinearLayout selectDatelayout;
+    @BindView(R.id.particularlayout)
+    LinearLayout particularlayout;
+    @BindView(R.id.openingbalnaceDebitValue)
+    TextView openingbalnaceDebitValue;
+    @BindView(R.id.openingbalnaceCreditValue)
+    TextView openingbalnaceCreditValue;
+    @BindView(R.id.topLayout)
+    LinearLayout topLayout;
+    @BindView(R.id.cashsaleDebitValue)
+    TextView cashsaleDebitValue;
+    @BindView(R.id.cashsaleCreditValue)
+    TextView cashsaleCreditValue;
+    @BindView(R.id.creditsaleDebitValue)
+    TextView creditsaleDebitValue;
+    @BindView(R.id.creditsaleCreditValue)
+    TextView creditsaleCreditValue;
+    @BindView(R.id.creditcardsaleDebitValue)
+    TextView creditcardsaleDebitValue;
+    @BindView(R.id.creditcardsaleCreditValue)
+    TextView creditcardsaleCreditValue;
+    @BindView(R.id.cashPurchaseDebitValue)
+    TextView cashPurchaseDebitValue;
+    @BindView(R.id.cashPurchaseCreditValue)
+    TextView cashPurchaseCreditValue;
+    @BindView(R.id.creditPurchaseDebitValue)
+    TextView creditPurchaseDebitValue;
+    @BindView(R.id.creditPurchaseCreditValue)
+    TextView creditPurchaseCreditValue;
+    @BindView(R.id.mainlayout)
+    LinearLayout mainlayout;
+    @BindView(R.id.totalDebitValue)
+    TextView totalDebitValue;
+    @BindView(R.id.totalCreditValue)
+    TextView totalCreditValue;
+    @BindView(R.id.closingBalnceDebitValue)
+    TextView closingBalnceDebitValue;
+    @BindView(R.id.closingBalnceCreditValue)
+    TextView closingBalnceCreditValue;
+    @BindView(R.id.bottomLayout)
+    LinearLayout bottomLayout;
+    Unbinder unbinder;
 
     public DayBookFragment() {
     }
@@ -28,7 +90,14 @@ public class DayBookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_day_book, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
 
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/scorg/farmaeasy/ui/fragments/ShortBookFragment.java
+++ app/src/main/java/com/scorg/farmaeasy/ui/fragments/ShortBookFragment.java
@@ -2,17 +2,47 @@ package com.scorg.farmaeasy.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.scorg.farmaeasy.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ShortBookFragment extends Fragment {
+
+    @BindView(R.id.fromDateValue)
+    TextView fromDateValue;
+    @BindView(R.id.fromDateMainLayout)
+    LinearLayout fromDateMainLayout;
+    @BindView(R.id.toDateValue)
+    TextView toDateValue;
+    @BindView(R.id.toDateMainLayout)
+    LinearLayout toDateMainLayout;
+    @BindView(R.id.selectDatelayout)
+    LinearLayout selectDatelayout;
+    @BindView(R.id.sortingSpinner)
+    AppCompatSpinner sortingSpinner;
+    @BindView(R.id.productslayout)
+    LinearLayout productslayout;
+    @BindView(R.id.shortBookList)
+    RecyclerView shortBookList;
+
+    Unbinder unbinder;
 
     public ShortBookFragment() {
     }
@@ -29,7 +59,28 @@ public class ShortBookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_short_book, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+
+        List<String> spinnerArray = new ArrayList<String>();
+
+        spinnerArray.add("Sort By");
+        spinnerArray.add("Product");
+        spinnerArray.add("Company");
+        spinnerArray.add("Supplier");
+        spinnerArray.add("Quantity");
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(getContext(), R.layout.sorting_spinner_item, spinnerArray);
+        aa.setDropDownViewResource(R.layout.sorting_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        sortingSpinner.setAdapter(aa);
 
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
